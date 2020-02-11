b4a5f676e49ef43b09479a0f96305193609de0c2
nampt68 <59510171+nampt68@users.noreply.github.com>
2020-02-12 01:18:59 +0900

    [101]-FPS Sample Project Setting - New Beginning
    [101]-FPS Sample Project Setting - New Beginning

---
+++ Library/PackageCache/com.unity.scriptablebuildpipeline@1.1.1-preview/Editor/Utilities/SceneStateCleanup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEditor.Build.Pipeline.Utilities;
+using UnityEditor.SceneManagement;
+
+namespace UnityEditor.Build.Utilities
+{
+    public class SceneStateCleanup : IDisposable
+    {
+        SceneSetup[] m_Scenes;
+
+        bool m_Disposed;
+
+        public SceneStateCleanup()
+        {
+            m_Scenes = EditorSceneManager.GetSceneManagerSetup();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (m_Disposed)
+                return;
+
+            m_Disposed = true;
+
+            if (disposing)
+            {
+                if (!m_Scenes.IsNullOrEmpty())
+                    EditorSceneManager.RestoreSceneManagerSetup(m_Scenes);
+                else
+                    EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects);
+            }
+        }
+    }
+}
